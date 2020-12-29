0ac1ed3d957254efd58eb546a52058eb599bd95c
Akosile Tayo <akosilety9@gmail.com>
2020-12-29 03:52:32 -0800

    Almost done with the desktop version

--- script/main.js
+++ script/main.js
@@ -4,7 +4,8 @@
     // HTML SELECTORS
     const burgerMenu = document.querySelector(".burgerMenu .material-icons"),
         navMenu = document.querySelector(".navMenu"),
-        darkOverlayBg = document.querySelector(".jsBodyOverlay"), phoneImage = document.querySelector(".main__article--img img");
+        darkOverlayBg = document.querySelector(".jsBodyOverlay"),
+        phoneImage = document.querySelector(".main__article--img img");
 
 
     // Click Event Listener
@@ -12,15 +13,27 @@
 
     // Function
     function menuOpen() {
+        // When it is clicked at first
         phoneImage.style.opacity = 0;
         phoneImage.style.transition = "0.3s ease-in-out opacity";
         console.log(phoneImage);
         navMenu.classList.toggle("active");
+
+
         if (navMenu.classList.contains("active")) {
+            //If nav is open,change the burger to x
             this.innerHTML = "close";
             darkOverlayBg.classList.add("active")
+            document.body.style.position = 'fixed';
+            document.body.style.top = `-${window.scrollY}px`;
             return
         }
+        //else,change it back to burger
+        // When the modal is hidden...
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
         phoneImage.style.opacity = 1;
         phoneImage.style.transition = "0.4s ease-out opacity";
         this.innerHTML = "menu"
